c642093d2827b78f7cfca0525a15f02951bf9ee7
victoria cavalcante <victoriacavalcantf@gmail.com>
2024-11-25 23:18:52 -0300

    exercicio três - soma int

---
+++ exercicio_tres.py
@@ -0,0 +1,7 @@
+# Faça um Programa que peça dois números e imprima a soma.
+
+num_one = int(input("insira o primeiro número: \n"))
+num_two = int(input("insira o segundo número: \n"))
+sum = num_one + num_two
+
+print("o valor da soma é: ", sum)
