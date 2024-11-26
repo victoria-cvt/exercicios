923cd88f4057e8f47be9ebf17836b200031f8030
victoria cavalcante <victoriacavalcantf@gmail.com>
2024-11-25 23:10:36 -0300

    exercicios basicos de python

---
+++ exercicio_dois.py
@@ -0,0 +1,4 @@
+#Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+number = int(input("Informe um número inteiro: "))
+print("O número escolhido foi: ", number)
---
+++ exercicio_um.py
@@ -0,0 +1,4 @@
+"Excercicios basicos de python"
+# Faça um código que mostre a mensagem "alô mundo!"
+
+var = print("hello world!")
